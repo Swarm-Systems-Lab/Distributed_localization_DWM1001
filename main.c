3f501f79856813fc7b1bfab5b80748981564f6a8
abvg9 <deveavg@gmail.com>
2021-03-10 18:07:36 +0100

    Added functions to power off leds.

--- main.c
+++ main.c
@@ -5,18 +5,18 @@ static THD_WORKING_AREA(waThread1, 64);
 static THD_FUNCTION(Thread1, arg) {
 
     (void)arg;
-    uint8_t green = GREEN_LED_D9;
+    //uint8_t green = GREEN_LED_D9;
     //uint8_t blue = BLUE_LED_D10;
-    //uint8_t red1 = RED_LED_D11;
+    uint8_t red1 = RED_LED_D11;
     //uint8_t red2 = RED_LED_D12;
 
     chRegSetThreadName("blinker");
 
     while (1) {
-      palTogglePad(IOPORT1, green);
-      //palTogglePad(IOPORT1, led2);
-      //palTogglePad(IOPORT1, led3);
-      //palTogglePad(IOPORT1, led4);
+      //palTogglePad(IOPORT1, green);
+      //palTogglePad(IOPORT1, blue);
+      palTogglePad(IOPORT1, red1);
+      //palTogglePad(IOPORT1, red2);
       chThdSleepMilliseconds(100);
     }
 }
@@ -26,10 +26,15 @@ int main(void)
     halInit();
     chSysInit();
 
-    palSetPad(IOPORT1, GREEN_LED_D9);
-    //palSetPad(IOPORT1, LED2);
-    //palSetPad(IOPORT1, LED3);
-    //palSetPad(IOPORT1, LED4);
+    palClearPad(IOPORT1, GREEN_LED_D9);
+    palClearPad(IOPORT1, BLUE_LED_D10);
+    palClearPad(IOPORT1, RED_LED_D11);
+    palClearPad(IOPORT1, RED_LED_D12);
+
+    //palSetPad(IOPORT1, GREEN_LED_D9);
+    //palSetPad(IOPORT1, BLUE_LED_D10);
+    palSetPad(IOPORT1, RED_LED_D11);
+    //palSetPad(IOPORT1, RED_LED_D12);
 
     chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO+1,
       Thread1, NULL);
