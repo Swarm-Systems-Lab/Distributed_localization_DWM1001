09acf835c7d75eca0a01db0c90e012dd544d5e7b
Hector G. de Marina <noeth3r@gmail.com>
2021-03-10 13:57:22 +0100

    main clean

--- main.c
+++ main.c
@@ -1,21 +1,6 @@
 #include "ch.h"
 #include "hal.h"
 
-void gpt_callback(GPTDriver *gptp) {
-  palTogglePad(IOPORT1, LED2);
-}
-
-/*
- * GPT configuration
- * Frequency: 31250Hz (32us period)
- * Resolution: 16 bits
- */
-static const GPTConfig gpt_config = {
-    .frequency  = 31250,
-    .callback   = gpt_callback,
-    .resolution = 16,
-};
-
 static THD_WORKING_AREA(waThread1, 64);
 static THD_FUNCTION(Thread1, arg) {
 
@@ -25,8 +10,8 @@ static THD_FUNCTION(Thread1, arg) {
     chRegSetThreadName("blinker");
 
     while (1) {
-    alTogglePad(IOPORT1, led);
-    chThdSleepMilliseconds(100);
+      palTogglePad(IOPORT1, led);
+      chThdSleepMilliseconds(100);
     }
 }
 
@@ -34,20 +19,13 @@ static THD_FUNCTION(Thread1, arg) {
  */
 int main(void)
 {
-
     halInit();
     chSysInit();
-    shellInit();
-
-    sdStart(&SD1, &serial_config);
 
     palSetPad(IOPORT1, LED1);
     palClearPad(IOPORT1, LED2);
     palClearPad(IOPORT1, LED3);
     palSetPad(IOPORT1, LED4);
-
-    gptStart(&GPTD1, &gpt_config);
-    gptStartContinuous(&GPTD1, 31250);
 
     chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO+1,
       Thread1, NULL);
