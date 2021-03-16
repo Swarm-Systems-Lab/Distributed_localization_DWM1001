dfd487b4774dcff34b4f2bdf1a64c575fe6502cb
abvg9 <deveavg@gmail.com>
2021-03-16 23:13:45 +0100

    Refactor code and implement conditial and event blink LEDs functions.

--- src/driver/led/led.c
+++ src/driver/led/led.c
@@ -1,6 +1,61 @@
 #include "led.h"
 
 const led LEDS[NUMBER_OF_LEDS] = {green, blue, red1, red2};
+const leds_struct ALL_LEDS = {NUMBER_OF_LEDS, LEDS};
+
+THD_FUNCTION(conditional_blinker_function, arg) {
+
+	conditional_blink_arguments* cba = (conditional_blink_arguments*) arg;
+
+	if(cba->lds.num_leds > 0 && cba->lds.num_leds < NUMBER_OF_LEDS) {
+		int i;
+		while((cba->cond_funct)(cba->conf_funct_args)) {
+
+			for(i = 0; i < cba->lds.num_leds; ++i) {
+				toggle_led(cba->lds.l[i]);
+			}
+			chThdSleepMilliseconds(cba->delay);
+		}
+	}
+}
+
+THD_FUNCTION(event_blinker_function, arg) {
+
+	event_blink_arguments* eba = (event_blink_arguments*) arg;
+
+	if(eba->lds.num_leds > 0 && eba->lds.num_leds < NUMBER_OF_LEDS) {
+
+		eba->register_events_funct(eba->register_events_funct_args);
+		int i;
+
+		do {
+
+			// Wait until attached events happens.
+			chEvtWaitAny(ALL_EVENTS);
+
+			for(i = 0; i < eba->lds.num_leds; ++i) {
+				led_on(eba->lds.l[i]);
+			}
+
+			chThdSleepMilliseconds(eba->delay);
+
+			for(i = 0; i < eba->lds.num_leds; ++i) {
+				led_off(eba->lds.l[i]);
+			}
+
+		}while(true);
+	}
+}
+
+thread_t* conditional_blink(conditional_blink_arguments* args, const tprio_t prio, const char *th_name) {
+	return chThdCreateFromHeap(NULL, THD_WORKING_AREA_SIZE(sizeof(conditional_blinker_function)),
+			th_name, prio, conditional_blinker_function, (void*)args);
+}
+
+thread_t* event_blink(event_blink_arguments* args, const tprio_t prio, const char *th_name) {
+	return chThdCreateFromHeap(NULL, THD_WORKING_AREA_SIZE(sizeof(event_blinker_function)),
+			th_name, prio, event_blinker_function, (void*)args);
+}
 
 void led_off(const led l) {
 	palSetPad(IOPORT1, l);
@@ -10,17 +65,17 @@ void led_on(const led l) {
 	palClearPad(IOPORT1, l);
 }
 
-void leds_off(void) {
+void leds_off(leds_struct leds) {
 	int i;
-	for(i = 0; i < NUMBER_OF_LEDS; ++i) {
-		led_off(LEDS[i]);
+	for(i = 0; i < leds.num_leds; ++i) {
+		led_off(leds.l[i]);
 	}
 }
 
-void leds_on(void) {
+void leds_on(leds_struct leds) {
 	int i;
-	for(i = 0; i < NUMBER_OF_LEDS; ++i) {
-		led_on(LEDS[i]);
+	for(i = 0; i < leds.num_leds; ++i) {
+		led_on(leds.l[i]);
 	}
 }
 
--- src/driver/led/led.h
+++ src/driver/led/led.h
@@ -3,11 +3,12 @@
 
 #include "ch.h"
 #include "hal.h"
+#include <stdbool.h>
 
-// Number of board leds.
+// Number of board LEDs.
 #define NUMBER_OF_LEDS 4
 
-// Enumerate of the board leds.
+// Enumerate of the board LEDs.
 typedef enum {
     green = GREEN_LED_D9,
     blue = BLUE_LED_D10,
@@ -15,12 +16,73 @@ typedef enum {
     red2 = RED_LED_D12,
 } led;
 
-/* Struct created so that when a thread is created that makes a led blink,
-   this struct is passed to it as an argument. */
+extern const led LEDS[NUMBER_OF_LEDS];
+
+typedef struct  {
+	int num_leds; // Number of LEDs to control.
+	const led* l; // LEDs to control.
+} leds_struct;
+
+extern const leds_struct ALL_LEDS;
+
+// Structure for conditional_blink function.
 typedef struct {
-	led l;
-	int delay;
-}blink_arguments;
+	leds_struct lds; // Information of the LEDs to control.
+	int delay; // Delay in milliseconds between turn off and turn LEDs.
+	bool (*cond_funct)(void*); // Makes blink a led until this function return false.
+	void* conf_funct_args; // Arguments that conditional function receives.
+} conditional_blink_arguments;
+
+// Structure for event_blink function.
+typedef struct {
+	leds_struct lds; // Information of the LEDs to control.
+	int delay; // Delay in milliseconds between turn off and turn LEDs.
+	void (*register_events_funct)(void*); // Function where the thread will attached to the required events.
+	void* register_events_funct_args; // Arguments of the register_events_funct.
+} event_blink_arguments;
+
+/**
+ * @brief Makes blink a led while function provided return false.
+ *
+ * @param[in] args: conditional_blink_arguments structure.
+ * @param[in] prio: Priority of the thread.
+ * @param[in] th_name: Name of the thread.
+ *
+ * @return thread_t*: Pointer to the created thread. If this returned value
+ *                    is NULL means there are no memory to allocate the thread.
+ *
+ * @example:
+ * 				led leds[2] = {green, blue};
+ * 				leds_struct ls = {2, leds};
+ * 				conditional_blink_arguments cba = {ls, 200, &function, &function_args};
+ * 				conditional_blink(&bag, NORMALPRIO+1, "blinker_green_blue_leds");
+ *
+ * @note: You must define a function with this header -> bool function(void* args);
+ *
+ */
+thread_t* conditional_blink(conditional_blink_arguments* args, tprio_t prio, const char *th_name);
+
+/**
+ * @brief Makes blink a led when some event/events happen.
+ *
+ * @param[in] args: event_blink_arguments structure.
+ * @param[in] prio: Priority of the thread.
+ * @param[in] th_name: Name of the thread.
+ *
+ * @return thread_t*: Pointer to the created thread. If this returned value
+ *                    is NULL means there are no memory to allocate the thread.
+ *
+ * @example:
+ * 				led leds[1] = {green};
+ * 				leds_struct ls = {1, leds};
+ * 				event_blink_arguments eba = {ls, 200, &function, function_args};
+ * 				conditional_blink(&eba, NORMALPRIO+1, "blinker_green_led");
+ *
+ * @note: You must define a function with this header -> void function(void* args);
+ * @note: In this function, you must attach the thread to the events.
+ *
+ */
+thread_t* event_blink(event_blink_arguments* args, tprio_t prio, const char *th_name);
 
 /**
  * @brief Power off a specific led.
@@ -39,16 +101,18 @@ void led_off(const led l);
 void led_on(const led l);
 
 /**
- * @brief Power off all led.
+ * @brief Power off specific LEDs.
+ * @param[in] leds: Information of which LEDs must turn off.
  *
  */
-void leds_off(void);
+void leds_off(leds_struct leds);
 
 /**
- * @brief Power on all led.
+ * @brief Power on specific LEDs.
+ * @param[in] leds: Information of which LEDs must turn on.
  *
  */
-void leds_on(void);
+void leds_on(leds_struct leds);
 
 /**
  * @brief Invert the value that the led had.
