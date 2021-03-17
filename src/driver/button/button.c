bf330d9a07226afa61a959147ddee6420972f169
abvg9 <deveavg@gmail.com>
2021-03-17 20:09:17 +0100

    Implemented button driver.

--- src/driver/button/button.c
+++ src/driver/button/button.c
@@ -1,5 +1,10 @@
 #include "button.h"
 
-bool read_button(button b) {
-    return palReadPad(IOPORT1, b);
+void set_button_cb(const button b, palcallback_t cb, void *args) {
+    palEnablePadEvent(IOPORT1, b, PAL_EVENT_MODE_BOTH_EDGES);
+    palSetPadCallback(IOPORT1, b, cb, args);
+}
+
+bool read_button(const button b) {
+    return !palReadPad(IOPORT1, b);
 }
--- src/driver/button/button.h
+++ src/driver/button/button.h
@@ -5,11 +5,31 @@
 #include "hal.h"
 #include <stdbool.h>
 
+// Enumerate of the board buttons.
 typedef enum {
-    bt1 = SW1,
-    bt2 = SW2,
+    bt1 = SW1, bt2 = SW2,
 } button;
 
-bool read_button(button b);
+/**
+ * @brief Bind function with a button. When the button is pressed,
+ *        the callback function, aka cb, will be called.
+ *
+ * @param[in] b: Button to bind with the cb function.
+ * @param[in] cb: Function that will be executed when the button is pressed.
+ * @param[in] args: Argument of the cb function.
+ *
+ */
+void set_button_cb(const button b, palcallback_t cb, void *args);
+
+/**
+ * @brief Check if a button was pressed.
+ *
+ * @param[in] b: Button to check.
+ *
+ * @return bool: True if the button was pressed in the reading moment.
+ *               Otherwise false.
+ *
+ */
+bool read_button(const button b);
 
 #endif // _BUTTON_
