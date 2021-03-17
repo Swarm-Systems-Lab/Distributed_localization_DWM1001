0dad1c76f5ca40bb3535df33e214c40abb67a905
abvg9 <deveavg@gmail.com>
2021-03-17 02:48:56 +0100

    Refactored and improve code and documentation.

--- src/driver/led/led.h
+++ src/driver/led/led.h
@@ -18,71 +18,90 @@ typedef enum {
 
 extern const led LEDS[NUMBER_OF_LEDS];
 
+// Structure that contains an array of LEDs.
 typedef struct  {
 	int num_leds; // Number of LEDs to control.
 	const led* l; // LEDs to control.
 } leds_struct;
 
+// Structure for blink function.
+typedef struct  {
+	leds_struct leds; // Number of LEDs to control.
+	int delay; // Delay in milliseconds between turn off and turn LEDs.
+} blink_leds_struct;
+
 extern const leds_struct ALL_LEDS;
 
 // Structure for conditional_blink function.
 typedef struct {
-	leds_struct lds; // Information of the LEDs to control.
-	int delay; // Delay in milliseconds between turn off and turn LEDs.
+	blink_leds_struct b_lds; // Information of the blinking LEDs to control.
 	bool (*cond_funct)(void*); // Makes blink a led until this function return false.
 	void* conf_funct_args; // Arguments that conditional function receives.
 } conditional_blink_arguments;
 
 // Structure for event_blink function.
 typedef struct {
-	leds_struct lds; // Information of the LEDs to control.
-	int delay; // Delay in milliseconds between turn off and turn LEDs.
+	blink_leds_struct b_lds; // Information of the blinking LEDs to control.
 	void (*register_events_funct)(void*); // Function where the thread will attached to the required events.
 	void* register_events_funct_args; // Arguments of the register_events_funct.
 } event_blink_arguments;
 
 /**
- * @brief Makes blink a led while function provided return false.
+ * @brief Makes LEDs blink.
+ *
+ * @param[in] leds: Leds to make blink.
+ *
+ */
+void blink(blink_leds_struct leds);
+
+/**
+ * @brief Makes blink LEDs while function provided return false.
  *
  * @param[in] args: conditional_blink_arguments structure.
  * @param[in] prio: Priority of the thread.
  * @param[in] th_name: Name of the thread.
  *
- * @return thread_t*: Pointer to the created thread. If this returned value
+ * @return thread_t*: Pointer to the created thread. If the returned value
  *                    is NULL means there are no memory to allocate the thread.
  *
  * @example:
  * 				led leds[2] = {green, blue};
  * 				leds_struct ls = {2, leds};
- * 				conditional_blink_arguments cba = {ls, 200, &function, &function_args};
- * 				conditional_blink(&bag, NORMALPRIO+1, "blinker_green_blue_leds");
+ *
+ * 				blink_leds_struct bls = {ls, 200};
+ *
+ * 				conditional_blink_arguments cba = {bls, &function, &function_args};
+ * 				conditional_blink(&cba, NORMALPRIO+1, "blinker_green_blue_leds");
  *
  * @note: You must define a function with this header -> bool function(void* args);
  *
  */
-thread_t* conditional_blink(conditional_blink_arguments* args, tprio_t prio, const char *th_name);
+thread_t* conditional_blink(conditional_blink_arguments* args, const tprio_t prio, const char *th_name);
 
 /**
- * @brief Makes blink a led when some event/events happen.
+ * @brief Makes blink a LEDs when some event/events happen.
  *
  * @param[in] args: event_blink_arguments structure.
  * @param[in] prio: Priority of the thread.
  * @param[in] th_name: Name of the thread.
  *
- * @return thread_t*: Pointer to the created thread. If this returned value
+ * @return thread_t*: Pointer to the created thread. If the returned value
  *                    is NULL means there are no memory to allocate the thread.
  *
  * @example:
  * 				led leds[1] = {green};
  * 				leds_struct ls = {1, leds};
- * 				event_blink_arguments eba = {ls, 200, &function, function_args};
+ *
+ * 				blink_leds_struct bls = {ls, 400};
+ *
+ * 				event_blink_arguments eba = {bls, &function, function_args};
  * 				conditional_blink(&eba, NORMALPRIO+1, "blinker_green_led");
  *
  * @note: You must define a function with this header -> void function(void* args);
  * @note: In this function, you must attach the thread to the events.
  *
  */
-thread_t* event_blink(event_blink_arguments* args, tprio_t prio, const char *th_name);
+thread_t* event_blink(event_blink_arguments* args, const tprio_t prio, const char *th_name);
 
 /**
  * @brief Power off a specific led.
@@ -105,14 +124,14 @@ void led_on(const led l);
  * @param[in] leds: Information of which LEDs must turn off.
  *
  */
-void leds_off(leds_struct leds);
+void leds_off(const leds_struct leds);
 
 /**
  * @brief Power on specific LEDs.
  * @param[in] leds: Information of which LEDs must turn on.
  *
  */
-void leds_on(leds_struct leds);
+void leds_on(const leds_struct leds);
 
 /**
  * @brief Invert the value that the led had.
