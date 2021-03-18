968334d6b128be9e494b37dc87ba5888dc9bae53
abvg9 <deveavg@gmail.com>
2021-03-18 18:49:47 +0100

    Fixed "Button driver needs improvements" -[*] Callback function now can execute sleep functions. -[*] Now the module provide a unbind cb function.

--- src/driver/button/button.c
+++ src/driver/button/button.c
@@ -1,10 +1,34 @@
 #include "button.h"
 
-void set_button_cb(const button b, palcallback_t cb, void *args) {
-    palEnablePadEvent(IOPORT1, b, PAL_EVENT_MODE_BOTH_EDGES);
-    palSetPadCallback(IOPORT1, b, cb, args);
+THD_FUNCTION( button_cb, arg) {
+
+    button_cb_arguments *bca = (button_cb_arguments*) arg;
+
+    palEnablePadEvent(IOPORT1, bca->bt, PAL_EVENT_MODE_BOTH_EDGES);
+
+    while (!chThdShouldTerminateX()) {
+
+        palWaitPadTimeout(IOPORT1, bca->bt, TIME_INFINITE);
+
+        if (button_was_pressed(bca->bt)) {
+            bca->cb(bca->cb_args);
+        }
+
+    }
+
 }
 
-bool read_button(const button b) {
+thread_t* bind_button_cb(const button_cb_arguments *bca, const int prio,
+        const char *cb_funct_name) {
+
+    return chThdCreateFromHeap(NULL, THD_WORKING_AREA_SIZE(sizeof(button_cb)),
+            cb_funct_name, prio, button_cb, (void*) bca);
+}
+
+bool button_was_pressed(const button b) {
     return !palReadPad(IOPORT1, b);
+}
+
+void unbind_cb(thread_t *cb_thread) {
+    chThdTerminate(cb_thread);
 }
--- src/driver/button/button.h
+++ src/driver/button/button.h
@@ -10,16 +10,35 @@ typedef enum {
     bt1 = SW1, bt2 = SW2,
 } button;
 
+// Structure for set_button_cb function.
+typedef struct {
+    button bt; // Button to attach cb.
+    void (*cb)(void*); // Function to execute when the button is pressed.
+    void *cb_args; // Arguments that cb function receives.
+} button_cb_arguments;
+
 /**
  * @brief Bind function with a button. When the button is pressed,
  *        the callback function, aka cb, will be called.
  *
- * @param[in] b: Button to bind with the cb function.
- * @param[in] cb: Function that will be executed when the button is pressed.
- * @param[in] args: Argument of the cb function.
+ * @param[in] bca: Button to bind with the cb function.
+ * @param[in] prio: Priority of the thread.
+ * @param[in] cb_funct_name: Name of the thread.
  *
+ * @return thread_t*: Pointer to the created thread. If the returned value
+ *                    is NULL means there are no memory to allocate the thread.
+ *
+ * @example:
+ *              button_cb_arguments bca = {bt1, function, &bls};
+ *
+ *              thread_t* th = set_button_cb(&bca, NORMALPRIO+1, "bt1_cb_function");
+ *
+ * @note: You must define a function with this header -> void function(void* args);
+ *
+ * @note: If you want to detach the cb function of the button, you must
+ *        store the return value of this function. See unbind_cb function.
  */
-void set_button_cb(const button b, palcallback_t cb, void *args);
+thread_t* bind_button_cb(const button_cb_arguments* bca, const int prio, const char *cb_funct_name);
 
 /**
  * @brief Check if a button was pressed.
@@ -30,6 +49,14 @@ void set_button_cb(const button b, palcallback_t cb, void *args);
  *               Otherwise false.
  *
  */
-bool read_button(const button b);
+bool button_was_pressed(const button b);
+
+/**
+ * @brief Unbind previously binded button cb function.
+ *
+ * @param[in] cb_thread: Pointer to the cb thread.
+ *
+ */
+void unbind_cb(thread_t* cb_thread);
 
 #endif // _BUTTON_
