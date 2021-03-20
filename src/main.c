18f4dc27c10c3b34433d1b0f204b0cf5f8460bc6
Hector G. de Marina <noeth3r@gmail.com>
2021-03-20 11:43:13 +0100

    only one main

--- src/main.c
+++ src/main.c
@@ -1,25 +1,14 @@
 #include "ch.h"
 #include "hal.h"
+#include "led.h"
+#include "button.h"
 
-static THD_WORKING_AREA(waThread1, 64);
+void function(void* args) {
+    toggle_led(blue);
+}
 
-static THD_FUNCTION(Thread1, arg) {
-
-    (void)arg;
-    //uint8_t green = GREEN_LED_D9;
-    //uint8_t blue = BLUE_LED_D10;
-    uint8_t red1 = RED_LED_D11;
-    //uint8_t red2 = RED_LED_D12;
-
-    chRegSetThreadName("blinker");
-
-    while (1) {
-      //palTogglePad(IOPORT1, green);
-      //palTogglePad(IOPORT1, blue);
-      palTogglePad(IOPORT1, red1);
-      //palTogglePad(IOPORT1, red2);
-      chThdSleepMilliseconds(100);
-    }
+void function2(void* args) {
+    toggle_led(green);
 }
 
 int main(void) {
@@ -27,25 +16,15 @@ int main(void) {
     halInit();
     chSysInit();
 
-    uint8_t green = GREEN_LED_D9;
-    uint8_t blue = BLUE_LED_D10;
-    uint8_t red1 = RED_LED_D11;
-    uint8_t red2 = RED_LED_D12;
+    leds_off(ALL_LEDS);
 
-    // Power off the leds.
-    palSetPad(IOPORT1, green); 
-    palSetPad(IOPORT1, blue);
-    palSetPad(IOPORT1, red1);
-    palSetPad(IOPORT1, red2);
-
-    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO+1,
-      Thread1, NULL);
-
-    chThdSleep(2);
-
-//    NRF_P0->DETECTMODE = 0;
+    button_cb_arguments bca = {bt1, function, NULL};
+    button_cb_arguments bca2 = {bt2, function2, NULL};
+    bind_button_cb(&bca, NORMALPRIO+1, "blinker buton");
+    bind_button_cb(&bca2, NORMALPRIO+1, "blinker buton");
 
     while (true) {
-        chThdSleepMilliseconds(250);
+       chThdSleepMilliseconds(250);
+       toggle_led(red1);
     }
 }
