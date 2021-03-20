c3b21f8fa010c1ce4c2af3572f978568f2e19078
Hector G. de Marina <noeth3r@gmail.com>
2021-03-20 16:57:42 +0100

    almost working example

--- src/main.c
+++ src/main.c
@@ -11,22 +11,30 @@ void radio_test_send_msg(void* args) {
     tx_msg.length = 1;
     tx_msg.pipe = 1;
     tx_msg.noack = 1;
-   // tx_msg.pid = 1;
+    tx_msg.pid = 1;
     tx_msg.data[0] = data;
 
-    nrf52_error_t error = radio_write_payload(&tx_msg);
-    switch(error){
-      case NRF52_SUCCESS:
-          toggle_led(blue);
-          break;
-      case NRF52_ERROR_INVALID_LENGTH:
-          toggle_led(red2);
-          break;
-      case NRF52_INVALID_STATE:
-          toggle_led(green);
-          break;
-      default:
-          break;
+    radio_write_payload(&tx_msg);
+}
+
+static THD_WORKING_AREA(waRadioListenerThread, 128);
+static THD_FUNCTION(RadioListenerThread, arg) {
+    event_listener_t radio_listener;
+    chEvtRegisterMaskWithFlags(&RFD1.eventsrc,
+                           &radio_listener,
+                           EVENT_MASK(0),
+                           NRF52_EVENT_RX_RECEIVED);
+    while(true){
+      eventmask_t evt = chEvtWaitAny(ALL_EVENTS);
+      if(evt & EVENT_MASK(0)) {
+          eventflags_t flags = chEvtGetAndClearFlags(&radio_listener);
+          if(flags & NRF52_EVENT_RX_RECEIVED){
+            nrf52_payload_t rx_msg;
+            radio_read_rx_payload(&rx_msg);
+              if(rx_msg.data[0] == 0xFE)
+                toggle_led(green);
+          }
+      }
     }
 }
 
@@ -60,8 +68,10 @@ int main(void) {
         RADIO_ESB_STATIC_PAYLOAD_LENGTH,
         nrf52_address};
 
-    if(NRF52_SUCCESS == radio_init(&nrf52_conf_init))
-        led_on(green);
+    radio_init(&nrf52_conf_init);
+
+    (void) chThdCreateStatic(waRadioListenerThread, sizeof(waRadioListenerThread),
+                                  NORMALPRIO + 3, RadioListenerThread, NULL);
 
     button_cb_arguments bca = {bt1, radio_test_send_msg, NULL};
     button_cb_arguments bca2 = {bt2, radio_test_send_msg, NULL};
