ad8357b5b064872f772d759cdbb31311e1c080bf
Hector G. de Marina <noeth3r@gmail.com>
2021-03-31 15:03:27 +0200

    driver led

--- src/main.c
+++ src/main.c
@@ -1,45 +1,29 @@
 #include "ch.h"
 #include "hal.h"
 
-static THD_WORKING_AREA(waThread1, 64);
-
-static THD_FUNCTION(Thread1, arg) {
-
-    (void)arg;
-    //uint8_t green = GREEN_LED_D9;
-    //uint8_t blue = BLUE_LED_D10;
-    uint8_t red1 = RED_LED_D11;
-    //uint8_t red2 = RED_LED_D12;
-
-    chRegSetThreadName("blinker");
-
-    while (1) {
-      //palTogglePad(IOPORT1, green);
-      //palTogglePad(IOPORT1, blue);
-      palTogglePad(IOPORT1, red1);
-      //palTogglePad(IOPORT1, red2);
-      chThdSleepMilliseconds(100);
-    }
-}
+#include "led.h"
+#include "nrf52_radio.h"
 
 int main(void) {
 
     halInit();
     chSysInit();
 
-    uint8_t green = GREEN_LED_D9;
-    uint8_t blue = BLUE_LED_D10;
-    uint8_t red1 = RED_LED_D11;
-    uint8_t red2 = RED_LED_D12;
+    leds_off(ALL_LEDS);
 
-    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO+1,
-      Thread1, NULL);
+    radio_init(&radiocfg);
+    radio_flush_tx();
+    radio_flush_rx();
+    radio_start_rx();
+
+//    chThdCreateStatic(waRadioThread, sizeof(waRadioThread), NORMALPRIO, RadioThread, NULL);
 
     chThdSleep(2);
 
 //    NRF_P0->DETECTMODE = 0;
 
     while (true) {
-        chThdSleepMilliseconds(250);
+        toggle_led(blue);
+        chThdSleepMilliseconds(500);
     }
 }
